e1f40467275c1dbbf1e57e1eb1dfcb7808b54471
Roflmuffin <shortguy014@gmail.com>
2023-10-09 17:32:51 +1000

    feat: make code gen more generic for future scripts

--- tooling/CodeGen.Natives/Program.cs
+++ tooling/CodeGen.Natives/Program.cs
@@ -14,8 +14,7 @@
  *  along with CounterStrikeSharp.  If not, see <https://www.gnu.org/licenses/>. *
  */
 
-using System.Text;
-using YamlDotNet.Serialization;
+using CodeGen.Natives.Scripts;
 
 namespace CodeGen.Natives
 {
@@ -23,99 +22,7 @@ namespace CodeGen.Natives
     {
         static void Main(string[] args)
         {
-            var pathToSearch = Path.Join(Helpers.GetRootDirectory(), "src/scripting/natives");
-
-            var deserializer = new DeserializerBuilder()
-                .Build();
-
-            var natives = new List<NativeDefinition>();
-            foreach (string file in Directory.EnumerateFiles(pathToSearch, "natives*yaml", SearchOption.AllDirectories))
-            {
-                var deserialized = deserializer.Deserialize<Dictionary<string, string>>(File.ReadAllText(file));
-
-                foreach (var nativeName in deserialized.Keys)
-                {
-                    var parts = deserialized[nativeName].Split(new string[] { "->" }, StringSplitOptions.None);
-                    var returnType = parts[1].Trim();
-                    var parameterString = parts[0].Trim();
-
-                    var parameters = new Dictionary<string, string>();
-
-                    if (!string.IsNullOrEmpty(parameterString))
-                    {
-                        // Get each parameter, then map its type to the dictionary
-                        // i.e. callback: pointer, name: string
-                        parameters = parameterString
-                            .Split(',')
-                            .Select(part => part.Split(':'))
-                            .ToDictionary(
-                                pair => pair[0].Trim().ToCamelCase(),
-                                pair => pair[1].Trim()
-                            );
-                    }
-
-                    natives.Add(new(nativeName, parameters, returnType));
-                }
-            }
-
-            var nativesString = string.Join("\n", natives.Select(native =>
-            {
-                var arguments = string.Join(", ",
-                    native.Arguments.Select(pair => $"{Mapping.GetCSharpType(pair.Value)} {pair.Key}"));
-
-                var returnStr = new StringBuilder($@"
-        public static {Mapping.GetCSharpType(native.ReturnType)} {native.NameCamelCase}({arguments}){{{Environment.NewLine}");
-
-                returnStr.Append("\t\t\tlock (ScriptContext.GlobalScriptContext.Lock) {\n");
-                returnStr.Append("\t\t\tScriptContext.GlobalScriptContext.Reset();\n");
-                foreach (var kv in native.Arguments)
-                {
-                    if (kv.Value == "object[]")
-                    {
-                        returnStr.Append($"\t\t\tforeach (var obj in {kv.Key})\n");
-                        returnStr.Append($"\t\t\t{{\n");
-                        returnStr.Append($"\t\t\t\tScriptContext.GlobalScriptContext.Push(obj);\n");
-                        returnStr.Append($"\t\t\t}}\n");
-                    }
-                    else
-                    {
-                        returnStr.Append(
-                            $"\t\t\tScriptContext.GlobalScriptContext.{Mapping.GetPushType(kv.Value)}{kv.Key});\n");
-                    }
-                }
-
-                returnStr.Append(
-                    $"\t\t\tScriptContext.GlobalScriptContext.SetIdentifier({$"0x{native.Hash:X}"});\n");
-                returnStr.Append("\t\t\tScriptContext.GlobalScriptContext.Invoke();\n");
-                returnStr.Append("\t\t\tScriptContext.GlobalScriptContext.CheckErrors();\n");
-
-                if (native.ReturnType != "void")
-                {
-                    returnStr.Append(
-                        $"\t\t\treturn ({Mapping.GetCSharpType(native.ReturnType)})ScriptContext.GlobalScriptContext.GetResult(typeof({Mapping.GetCSharpType(native.ReturnType)}));\n");
-                }
-
-                returnStr.Append("\t\t\t}\n");
-                returnStr.Append("\t\t}");
-
-                return returnStr.ToString();
-            }));
-
-            var result = $@"
-using System;
-
-namespace CounterStrikeSharp.API.Core
-{{
-    public class NativeAPI {{
-        {nativesString}
-    }}
-}}
-";
-
-            Console.WriteLine($"Generated C# bindings for {natives.Count} methods successfully.");
-
-            File.WriteAllText(Path.Join(Helpers.GetRootDirectory(), "managed/CounterStrikeSharp.API/Core/API.cs"),
-                result);
+            Generators.GenerateNatives();
         }
     }
 }
