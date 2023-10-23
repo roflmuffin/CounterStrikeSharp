369e7c6aa9905dce03e87719ba47754a75fc2d0a
Roflmuffin <shortguy014@gmail.com>
2023-10-23 23:46:10 +1000

    feat: add `CommitSuicide` vfunc method

---
+++ managed/CounterStrikeSharp.API/Core/Model/CBasePlayerPawn.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using CounterStrikeSharp.API.Modules.Memory;
+
+namespace CounterStrikeSharp.API.Core;
+
+public partial class CBasePlayerPawn
+{
+    public void CommitSuicide(bool explode, bool force)
+    {
+        VirtualFunction.CreateVoid<IntPtr, bool, bool>(Handle, 354)(Handle, explode, force);
+    }
+}
