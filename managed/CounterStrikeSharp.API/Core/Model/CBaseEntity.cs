6798244684dc648b6c066bcfeebb1c2a09e83417
Roflmuffin <shortguy014@gmail.com>
2023-10-28 23:08:12 +1000

    feat; add teleport function

--- managed/CounterStrikeSharp.API/Core/Model/CBaseEntity.cs
+++ managed/CounterStrikeSharp.API/Core/Model/CBaseEntity.cs
@@ -1,4 +1,5 @@
-﻿using CounterStrikeSharp.API.Modules.Memory;
+﻿using System;
+using CounterStrikeSharp.API.Modules.Memory;
 using CounterStrikeSharp.API.Modules.Utils;
 
 namespace CounterStrikeSharp.API.Core;
@@ -6,4 +7,10 @@ namespace CounterStrikeSharp.API.Core;
 public partial class CBaseEntity
 {
     public Vector AbsVelocity => Schema.GetDeclaredClass<Vector>(this.Handle, "CBaseEntity", "m_vecAbsVelocity");
+    
+    public void Teleport(Vector position, QAngle angles, Vector velocity)
+    {
+        VirtualFunction.CreateVoid<IntPtr, IntPtr, IntPtr, IntPtr>(Handle, GameData.GetOffset("CBaseEntity_Teleport"))(
+            Handle, position.Handle, angles.Handle, velocity.Handle);
+    }
 }
