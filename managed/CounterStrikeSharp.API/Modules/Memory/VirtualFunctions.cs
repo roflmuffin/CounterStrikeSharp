7d73453b76eb686fae8706083259d64be1e779db
Muinez <stasik9893@gmail.com>
2023-11-04 19:31:02 +0300

    Modified GiveNamedItem to return created entity pointer

--- managed/CounterStrikeSharp.API/Modules/Memory/VirtualFunctions.cs
+++ managed/CounterStrikeSharp.API/Modules/Memory/VirtualFunctions.cs
@@ -15,11 +15,12 @@ public static class VirtualFunctions
             GameData.GetSignature("UTIL_ClientPrintAll"));
 
     // void (*FnGiveNamedItem)(void* itemService,const char* pchName, void* iSubType,void* pScriptItem, void* a5,void* a6) = nullptr;
-    public static Action<IntPtr, string, IntPtr, IntPtr, IntPtr, IntPtr> GiveNamedItem =
-        VirtualFunction.CreateVoid<IntPtr, string, IntPtr, IntPtr, IntPtr, IntPtr>(
+    public static Func<IntPtr, string, IntPtr, IntPtr, IntPtr, IntPtr, IntPtr> GiveNamedItem =
+        VirtualFunction.Create<IntPtr, string, IntPtr, IntPtr, IntPtr, IntPtr, IntPtr>(
             GameData.GetSignature("GiveNamedItem"));
 
-    public static Action<IntPtr, byte> SwitchTeam = VirtualFunction.CreateVoid<IntPtr, byte>(GameData.GetSignature("CCSPlayerController_SwitchTeam"));
+    public static Action<IntPtr, byte> SwitchTeam =
+        VirtualFunction.CreateVoid<IntPtr, byte>(GameData.GetSignature("CCSPlayerController_SwitchTeam"));
 
     // void(*UTIL_Remove)(CEntityInstance*);
     public static Action<IntPtr> UTIL_Remove = VirtualFunction.CreateVoid<IntPtr>(GameData.GetSignature("UTIL_Remove"));
