caebafd568eb527d3aac55646856bcecf4b76882
Roflmuffin <shortguy014@gmail.com>
2023-11-07 17:01:04 +1000

    feat: add `MinimumApiVersion` attribute

---
+++ managed/CounterStrikeSharp.API/Api.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using CounterStrikeSharp.API.Core;
+
+namespace CounterStrikeSharp.API;
+
+public class Api
+{
+    /// <summary>
+    /// Returns the API version of CounterStrikeSharp running on the server
+    /// </summary>
+    /// <returns></returns>
+    public static int GetVersion()
+    {
+        return Assembly.GetAssembly(typeof(BasePlugin))!.GetName().Version!.Build;
+    }
+}
