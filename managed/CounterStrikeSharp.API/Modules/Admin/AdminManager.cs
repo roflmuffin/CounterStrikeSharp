95b5ccaa50f0b1e62b8ec75f1025e87229da56ee
zonical <zonicalguy@gmail.com>
2023-11-09 11:25:18 +1300

    Remove these

--- managed/CounterStrikeSharp.API/Modules/Admin/AdminManager.cs
+++ managed/CounterStrikeSharp.API/Modules/Admin/AdminManager.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.Json.Serialization;
 using System.Text.Json;
-using System.Threading.Tasks;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Modules.Entities;
 
