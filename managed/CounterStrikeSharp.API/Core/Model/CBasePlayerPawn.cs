f037810acd1c186ec24aa5996f6a0dfbbfda5bd0
KillStr3aK <statser07@gmail.com>
2023-11-22 17:10:07 +0100

    feat: player respawn

--- managed/CounterStrikeSharp.API/Core/Model/CBasePlayerPawn.cs
+++ managed/CounterStrikeSharp.API/Core/Model/CBasePlayerPawn.cs
@@ -5,6 +5,11 @@ namespace CounterStrikeSharp.API.Core;
 
 public partial class CBasePlayerPawn
 {
+    /// <summary>
+    /// Force player suicide
+    /// </summary>
+    /// <param name="explode"></param>
+    /// <param name="force"></param>
     public void CommitSuicide(bool explode, bool force)
     {
         VirtualFunction.CreateVoid<IntPtr, bool, bool>(Handle,  GameData.GetOffset("CBasePlayerPawn_CommitSuicide"))(Handle, explode, force);
