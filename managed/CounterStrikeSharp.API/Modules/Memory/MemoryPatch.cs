0b42e791b9b4cb9af92b5d938f4e54313095b1e0
KillStr3aK <statser07@gmail.com>
2023-12-01 12:48:15 +0100

    added wrapper class on managed

---
+++ managed/CounterStrikeSharp.API/Modules/Memory/MemoryPatch.cs
@@ -0,0 +1,42 @@
+/*
+ *  This file is part of CounterStrikeSharp.
+ *  CounterStrikeSharp is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  CounterStrikeSharp is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with CounterStrikeSharp.  If not, see <https://www.gnu.org/licenses/>. *
+ */
+
+public partial class MemoryPatch
+{
+    private readonly string _patchName;
+
+    private readonly string _patchSignature;
+
+    public readonly string PatchName => _patchName;
+
+    public readonly string Signature => _patchSignature;
+
+    public MemoryPatch(string patchName, string patchSignature)
+    {
+        _patchName = patchName;
+        _patchSignature = patchSignature;
+    }
+
+    public bool PerformPatch()
+    {
+        return NativeAPI.CreateMemoryPatch(_patchName, _patchSignature);
+    }
+
+    public void UndoPatch()
+    {
+        return NativeAPI.UndoMemoryPatch(_patchName);
+    }
+}
