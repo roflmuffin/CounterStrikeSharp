f8705ce04b09a8febfa8a72160d3b3ac30750ddb
Stimayk <51941742+Stimayk@users.noreply.github.com>
2024-01-04 18:37:25 +0400

    Update ChatMenu.cs
    On the advice of users added made all 4 menu outputs.

--- managed/CounterStrikeSharp.API/Modules/Menu/ChatMenu.cs
+++ managed/CounterStrikeSharp.API/Modules/Menu/ChatMenu.cs
@@ -1,32 +1,42 @@
+using CounterStrikeSharp.API.Modules.Utils;
 using System.Collections.Generic;
 using System.Text;
 using static CounterStrikeSharp.API.Core.Listeners;
 
 namespace CounterStrikeSharp.API.Modules.Menu
 {
+    public enum DisplayMethod
+    {
+        Chat,
+        CenterHtml,
+        Center,
+        Console
+    }
+
     public class ChatMenuOption
     {
+        public Action<CCSPlayerController, ChatMenuOption> OnSelect { get; set; }
+        public string Text { get; set; }
+        public bool Disabled { get; set; }
+
         public ChatMenuOption(string text, bool disabled, Action<CCSPlayerController, ChatMenuOption> onSelect)
         {
             Text = text;
             Disabled = disabled;
             OnSelect = onSelect;
         }
-
-        public Action<CCSPlayerController, ChatMenuOption> OnSelect { get; set; }
-
-        public string Text { get; set; }
-        public bool Disabled { get; set; }
     }
 
     public class ChatMenu
     {
         public string Title { get; set; }
         public List<ChatMenuOption> MenuOptions { get; } = new();
+        public DisplayMethod DisplayMethod { get; private set; }
 
-        public ChatMenu(string title)
+        public ChatMenu(string title, DisplayMethod displayMethod)
         {
             Title = title;
+            DisplayMethod = displayMethod;
         }
 
         public ChatMenuOption AddMenuOption(string display, Action<CCSPlayerController, ChatMenuOption> onSelect, bool disabled = false)
@@ -39,14 +49,13 @@ namespace CounterStrikeSharp.API.Modules.Menu
 
     public class ChatMenuInstance : BasePlugin
     {
-        readonly int _numPerPage = 4;
+        private readonly int _numPerPage = 6;
         private readonly Stack<int> _prevPageOffsets = new();
         private readonly ChatMenu _menu;
-        private Stack<ChatMenu> _menuStack = new Stack<ChatMenu>();
+        private CCSPlayerController? _player;
 
         int _page = 0;
         int _currentOffset = 0;
-        private CCSPlayerController? _player;
 
         public ChatMenuInstance(CCSPlayerController player, ChatMenu menu)
         {
@@ -54,7 +63,6 @@ namespace CounterStrikeSharp.API.Modules.Menu
             _player = player;
 
             RegisterListener<OnTick>(OnTick);
-            Display();
         }
 
         private bool HasPrevButton => _page > 0;
@@ -65,9 +73,152 @@ namespace CounterStrikeSharp.API.Modules.Menu
 
         public override string ModuleVersion => throw new NotImplementedException();
 
-        private void OnTick()
+        public void Display()
         {
-            Display();
+            if (_menu.DisplayMethod == DisplayMethod.Chat)
+            {
+                DisplayChat();
+            }
+            else if (_menu.DisplayMethod == DisplayMethod.CenterHtml)
+            {
+                DisplayCenterHtml();
+            }
+            else if (_menu.DisplayMethod == DisplayMethod.Center)
+            {
+                DisplayCenter();
+            }
+            else if (_menu.DisplayMethod == DisplayMethod.Console)
+            {
+                DisplayConsole();
+            }
+        }
+
+        private void DisplayChat()
+        {
+            _player?.PrintToChat(_menu.Title);
+            _player?.PrintToChat("---");
+
+            int keyOffset = 1;
+
+            if (HasPrevButton)
+            {
+                _player?.PrintToChat($" {ChatColors.Yellow}!1 {ChatColors.Default}-> Prev");
+                keyOffset++;
+            }
+
+            for (int i = _currentOffset;
+                 i < Math.Min(_currentOffset + MenuItemsPerPage, _menu.MenuOptions.Count);
+                 i++)
+            {
+                var option = _menu.MenuOptions[i];
+
+                _player?.PrintToChat(
+                    $" {(option.Disabled ? ChatColors.Grey : ChatColors.Green)} !{keyOffset++} {ChatColors.Default}{option.Text}");
+            }
+
+            if (HasNextButton)
+            {
+                _player?.PrintToChat($" {ChatColors.Yellow}!8 {ChatColors.Default}-> Next");
+            }
+        }
+
+        private void DisplayCenterHtml()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.AppendFormat("<b><font color='yellow'>{0}</font></b>", _menu.Title);
+            builder.AppendLine("<br>");
+
+            int keyOffset = 1;
+
+            if (HasPrevButton)
+            {
+                builder.AppendFormat("<font color='yellow'>!1 -> Prev</font>");
+                builder.AppendLine("<br>");
+                keyOffset++;
+            }
+
+            for (int i = _currentOffset; i < Math.Min(_currentOffset + MenuItemsPerPage, _menu.MenuOptions.Count); i++)
+            {
+                var option = _menu.MenuOptions[i];
+                builder.AppendFormat("<font color='red'>!{0}</font> {1}", keyOffset++, option.Text);
+                builder.AppendLine("<br>");
+            }
+
+            if (HasNextButton)
+            {
+                builder.AppendFormat("<font color='yellow'>!8 -> Next</font>");
+                builder.AppendLine("<br>");
+            }
+
+            builder.AppendFormat("<font color='red'>!9 -> Close</font>");
+            builder.AppendLine("<br>");
+
+            var currentPageText = builder.ToString();
+            _player?.PrintToCenterHtml(currentPageText);
+        }
+
+        private void DisplayCenter()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.AppendFormat("{0}", _menu.Title);
+            builder.AppendLine();
+
+            int keyOffset = 1;
+
+            if (HasPrevButton)
+            {
+                builder.AppendFormat("!1 -> Prev");
+                builder.AppendLine();
+                keyOffset++;
+            }
+
+            for (int i = _currentOffset; i < Math.Min(_currentOffset + MenuItemsPerPage, _menu.MenuOptions.Count); i++)
+            {
+                var option = _menu.MenuOptions[i];
+                builder.AppendFormat("!{0} {1}", keyOffset++, option.Text);
+                builder.AppendLine();
+            }
+
+            if (HasNextButton)
+            {
+                builder.AppendFormat("!8 -> Next");
+                builder.AppendLine();
+            }
+
+            builder.AppendFormat("!9 -> Close");
+            builder.AppendLine();
+
+            var currentPageText = builder.ToString();
+            _player?.PrintToCenter(currentPageText);
+        }
+
+        private void DisplayConsole()
+        {
+            _player?.PrintToConsole(_menu.Title);
+            _player?.PrintToConsole("---");
+
+            int keyOffset = 1;
+
+            if (HasPrevButton)
+            {
+                _player?.PrintToConsole($"!1 -> Prev");
+                keyOffset++;
+            }
+
+            for (int i = _currentOffset;
+                 i < Math.Min(_currentOffset + MenuItemsPerPage, _menu.MenuOptions.Count);
+                 i++)
+            {
+                var option = _menu.MenuOptions[i];
+
+                _player?.PrintToConsole(
+                    $" {(option.Disabled ? "[Enabled]" : "[Disabled] - ")} !{keyOffset++} {option.Text}");
+            }
+
+            if (HasNextButton)
+            {
+                _player?.PrintToConsole($"!8 -> Next");
+            }
         }
 
         internal void OnKeyPress(CCSPlayerController player, int key)
@@ -109,6 +260,14 @@ namespace CounterStrikeSharp.API.Modules.Menu
             }
         }
 
+        private void OnTick()
+        {
+            if (_menu.DisplayMethod == DisplayMethod.CenterHtml)
+            {
+                Display();
+            }
+        }
+
         public void Reset()
         {
             _currentOffset = 0;
@@ -127,65 +286,27 @@ namespace CounterStrikeSharp.API.Modules.Menu
 
         public void PrevPage()
         {
-            if (_prevPageOffsets.Count > 0)
-            {
-                _page--;
-                _currentOffset = _prevPageOffsets.Pop();
-                Display();
-            }
-        }
-
-        private void Display()
-        {
-            StringBuilder builder = new StringBuilder();
-            builder.AppendFormat("<b><font color='yellow'>{0}</font></b>", _menu.Title);
-            builder.AppendLine("<br>");
-
-            int keyOffset = 1;
-
-            if (HasPrevButton)
-            {
-                builder.AppendFormat("<font color='yellow'>!1 -> Prev</font>");
-                builder.AppendLine("<br>");
-                keyOffset++;
-            }
-
-            for (int i = _currentOffset; i < Math.Min(_currentOffset + MenuItemsPerPage, _menu.MenuOptions.Count); i++)
-            {
-                var option = _menu.MenuOptions[i];
-                builder.AppendFormat("<font color='red'>!{0}</font> {1}", keyOffset++, option.Text);
-                builder.AppendLine("<br>");
-            }
-
-            if (HasNextButton)
-            {
-                builder.AppendFormat("<font color='yellow'>!8 -> Next</font>");
-                builder.AppendLine("<br>");
-            }
-
-            builder.AppendFormat("<font color='red'>!9 -> Close</font>");
-            builder.AppendLine("<br>");
-
-            var currentPageText = builder.ToString();
-            _player?.PrintToCenterHtml(currentPageText);
+            _page--;
+            _currentOffset = _prevPageOffsets.Pop();
+            Display();
         }
     }
 
     public static class ChatMenus
     {
-        private static readonly Dictionary<int, ChatMenuInstance> ActiveMenus = new();
+        private static readonly Dictionary<IntPtr, ChatMenuInstance> ActiveMenus = new();
 
         public static void OpenMenu(CCSPlayerController player, ChatMenu menu)
         {
-            ActiveMenus[(int)player.Handle] = new ChatMenuInstance(player, menu);
+            ActiveMenus[player.Handle] = new ChatMenuInstance(player, menu);
+            ActiveMenus[player.Handle].Display();
         }
 
         public static void OnKeyPress(CCSPlayerController player, int key)
         {
-            if (ActiveMenus.TryGetValue((int)player.Handle, out var activeMenu))
-            {
-                activeMenu.OnKeyPress(player, key);
-            }
+            if (!ActiveMenus.ContainsKey(player.Handle)) return;
+
+            ActiveMenus[player.Handle].OnKeyPress(player, key);
         }
     }
 }
