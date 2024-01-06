77ca3f6cce81792cd0475dbb17446b3da8f4cf44
B3none <24966460+B3none@users.noreply.github.com>
2024-01-06 05:56:17 +0000

    Remove redundant using

--- managed/CounterStrikeSharp.API/Core/Plugin/PluginContext.cs
+++ managed/CounterStrikeSharp.API/Core/Plugin/PluginContext.cs
@@ -14,7 +14,6 @@
  *  along with CounterStrikeSharp.  If not, see <https://www.gnu.org/licenses/>. *
  */
 
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
