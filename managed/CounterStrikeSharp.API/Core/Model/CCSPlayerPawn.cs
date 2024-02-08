f2b8044b8c7b6973c3facf05b21a42f261973cf0
ZoNiCaL <zonicalguy@gmail.com>
2024-02-08 19:03:15 +1300

    Update gamedata for Call To Arms Update (#311)
    Co-authored-by: Michael Wilson <roflmuffin@users.noreply.github.com>

--- managed/CounterStrikeSharp.API/Core/Model/CCSPlayerPawn.cs
+++ managed/CounterStrikeSharp.API/Core/Model/CCSPlayerPawn.cs
@@ -1,19 +1,21 @@
 ﻿using System;
 using CounterStrikeSharp.API.Modules.Memory;
+using Microsoft.Extensions.Logging;
 
 namespace CounterStrikeSharp.API.Core;
 
 public partial class CCSPlayerPawn
 {
-    /// <summary>
-    /// Respawn player
-    /// </summary>
-    public void Respawn()
-    {
-        if (!Controller.IsValid) return;
-        if (!Controller.Value.IsValid) return;
+	/// <summary>
+	/// Respawn player
+	/// </summary>
 
-        VirtualFunctions.CCSPlayerPawn_Respawn(Handle);
-        VirtualFunction.CreateVoid<IntPtr>(Controller.Value.Handle, GameData.GetOffset("CCSPlayerController_Respawn"))(Controller.Value.Handle);
-    }
+	[Obsolete("Use CCSPlayerController.Respawn() instead")]
+	public void Respawn()
+	{
+		if (!Controller.IsValid) return;
+		if (!Controller.Value.IsValid) return;
+
+		Application.Instance.Logger.LogWarning("CCSPawn.Respawn is deprecated and does nothing, use CCSPlayerController.Respawn instead");
+	}
 }
