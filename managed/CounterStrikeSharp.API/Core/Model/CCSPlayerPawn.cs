98607dd6521158e82d424dd7d92fa8e7be6ed32d
zonical <zonicalguy@gmail.com>
2024-02-08 18:53:38 +1300

    Readd CCSPlayerPawn.Respawn (for now...)

---
+++ managed/CounterStrikeSharp.API/Core/Model/CCSPlayerPawn.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CounterStrikeSharp.API.Modules.Memory;
+
+namespace CounterStrikeSharp.API.Core;
+
+public partial class CCSPlayerPawn
+{
+	/// <summary>
+	/// Respawn player
+	/// </summary>
+
+	[Obsolete]
+	public void Respawn()
+	{
+		if (!Controller.IsValid) return;
+		if (!Controller.Value.IsValid) return;
+
+		// bit of a bodge ¯\_(ツ)_/¯
+		Controller.Value.As<CCSPlayerController>().Respawn();
+	}
+}
