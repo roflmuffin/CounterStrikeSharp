72178c95980aacfea8b2128930f5ae9321db3d58
Michael Wilson <roflmuffin@users.noreply.github.com>
2024-02-24 14:56:56 +1000

    feat: add chat color method for player controller (#339)

---
+++ managed/CounterStrikeSharp.API/Modules/Extensions/TeamExtensions.cs
@@ -0,0 +1,15 @@
+﻿using CounterStrikeSharp.API.Modules.Utils;
+
+namespace CounterStrikeSharp.API.Modules.Extensions;
+
+public static class TeamExtensions
+{
+    /// <summary>
+    /// <inheritdoc cref="ChatColors.ForTeam"/>
+    /// </summary>
+    /// <returns></returns>
+    public static char GetChatColor(this CsTeam team)
+    {
+        return ChatColors.ForTeam(team);
+    }
+}
