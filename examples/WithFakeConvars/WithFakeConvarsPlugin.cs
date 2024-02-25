8b952d3ba12054ff765c165b1eca695674f880e9
Roflmuffin <shortguy014@gmail.com>
2024-02-25 14:51:29 +1000

    feat: add protected, cheat & replicated flags

--- examples/WithFakeConvars/WithFakeConvarsPlugin.cs
+++ examples/WithFakeConvars/WithFakeConvarsPlugin.cs
@@ -19,15 +19,28 @@ public class WithFakeConvarsPlugin : BasePlugin
     public FakeConVar<bool> BoolCvar = new("example_bool", "An example boolean cvar", true);
     
     // Range validator is an inbuilt validator that can be used to ensure that a value is within a certain range.
-    public FakeConVar<int> ExampleIntCvar  = new("example_int", "An example integer cvar", 10, new RangeValidator<int>(0, 100));
+    public FakeConVar<int> ExampleIntCvar  = new("example_int", "An example integer cvar", 10, flags: ConVarFlags.FCVAR_NONE, new RangeValidator<int>(0, 100));
     
-    public FakeConVar<float> ExampleFloatCvar = new("example_float", "An example float cvar", 10, new RangeValidator<float>(5, 20));
+    public FakeConVar<float> ExampleFloatCvar = new("example_float", "An example float cvar", 10, flags: ConVarFlags.FCVAR_NONE, new RangeValidator<float>(5, 20));
     public FakeConVar<string> ExampleStringCvar = new("example_string", "An example string cvar", "default");
+
+    // Replicated, Cheat & Protected flags are supported.
+    public FakeConVar<float> ExamplePublicCvar = new("example_public_float", "An example public float cvar", 5,
+        ConVarFlags.FCVAR_REPLICATED);
+    
+    // Can only be changed if sv_cheats is enabled.
+    public FakeConVar<float> ExampleCheatCvar = new("example_cheat_float", "An example cheat float cvar", 5,
+        ConVarFlags.FCVAR_CHEAT);
+    
+    // Protected cvars do not output their value when queried.
+    public FakeConVar<float> ExampleProtectedCvar = new("example_protected_float", "An example cheat float cvar", 5,
+        ConVarFlags.FCVAR_PROTECTED);
     
     // You can create your own custom validators by implementing the IValidator interface.
-    public FakeConVar<int> ExampleEvenNumberCvar = new("example_even_number", "An example even number cvar", 0, new EvenNumberValidator());
+    public FakeConVar<int> ExampleEvenNumberCvar = new("example_even_number", "An example even number cvar", 0, flags: ConVarFlags.FCVAR_NONE, new EvenNumberValidator());
 
     public FakeConVar<int> RequiresRestartCvar = new("example_requires_restart", "A cvar that requires a restart when changed");
+    
     public override void Load(bool hotReload)
     {
         // You can subscribe to the ValueChanged event to execute code when the value of a cvar changes.
--- managed/CounterStrikeSharp.API/Modules/Cvars/FakeConVar.cs
+++ managed/CounterStrikeSharp.API/Modules/Cvars/FakeConVar.cs
@@ -9,18 +9,21 @@ public class FakeConVar<T> where T : IComparable<T>
 {
     private readonly IEnumerable<IValidator<T>>? _customValidators;
 
-    public FakeConVar(string name, string description, T defaultValue = default(T),
+    public FakeConVar(string name, string description, T defaultValue = default(T), ConVarFlags flags = ConVarFlags.FCVAR_NONE,
         params IValidator<T>[] customValidators)
     {
         _customValidators = customValidators;
         Name = name;
         Description = description;
         Value = defaultValue;
+        Flags = flags;
     }
+
+    public ConVarFlags Flags { get; set; }
 
     public string Name { get; }
     public string Description { get; }
-    
+
     public event EventHandler<T> ValueChanged;
 
     private T _value;
@@ -33,10 +36,33 @@ public class FakeConVar<T> where T : IComparable<T>
 
     internal void ExecuteCommand(CCSPlayerController? player, CommandInfo args)
     {
+        if (player != null && !Flags.HasFlag(ConVarFlags.FCVAR_REPLICATED))
+        {
+            return;
+        }
+
         if (args.ArgCount < 2)
         {
-            args.ReplyToCommand($"{args.GetArg(0)} = {Value.ToString()}");
+            if (Flags.HasFlag(ConVarFlags.FCVAR_PROTECTED))
+            {
+                args.ReplyToCommand($"{args.GetArg(0)} = <protected>");
+            }
+            else
+            {
+                args.ReplyToCommand($"{args.GetArg(0)} = {Value.ToString()}");
+            }
+
             return;
+        }
+        
+        if (Flags.HasFlag(ConVarFlags.FCVAR_CHEAT))
+        {
+            var cheats = ConVar.Find("sv_cheats")!.GetPrimitiveValue<bool>();
+            if (!cheats)
+            {
+                args.ReplyToCommand($"SV: Convar '{Name}' is cheat protected, change ignored");
+                return; 
+            }
         }
         
         if (player != null)
@@ -52,16 +78,16 @@ public class FakeConVar<T> where T : IComparable<T>
             TypeConverter converter = TypeDescriptor.GetConverter(typeof(T));
             if (converter.CanConvertFrom(typeof(string)))
             {
-                try 
+                try
                 {
                     parsedValue = (T)converter.ConvertFromString(args.ArgString);
-                } 
+                }
                 catch
                 {
                     success = typeof(T) == typeof(bool) && TryConvertCustomBoolean(args.ArgString, out parsedValue);
                 }
             }
-            
+
             if (!success)
             {
                 args.ReplyToCommand($"Error: String '{args.GetArg(1)}' can't be converted to {typeof(T).Name}");
@@ -76,21 +102,22 @@ public class FakeConVar<T> where T : IComparable<T>
             args.ReplyToCommand($"Error: {ex.Message}");
         }
     }
-    
-    private bool TryConvertCustomBoolean(string input, out T result) 
+
+    private bool TryConvertCustomBoolean(string input, out T result)
     {
         input = input.Trim().ToLowerInvariant();
-        if (input == "1" || input == "true") 
+        if (input == "1" || input == "true")
         {
             result = (T)(object)true;
             return true;
-        } 
+        }
         else if (input == "0" || input == "false")
         {
             result = (T)(object)false;
             return true;
         }
-        result = default(T); 
+
+        result = default(T);
         return false;
     }
 
