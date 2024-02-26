be58332ec1ad65c5e135e6e8f6b293f05a8b2001
Roflmuffin <shortguy014@gmail.com>
2024-02-26 16:27:52 +1000

    fix: change types in `PlayerCapability`

--- managed/CounterStrikeSharp.API/Core/Capabilities/PlayerCapability.cs
+++ managed/CounterStrikeSharp.API/Core/Capabilities/PlayerCapability.cs
@@ -5,14 +5,14 @@ namespace CounterStrikeSharp.API.Core.Capabilities;
 public sealed class PlayerCapability<T> where T : class
 {
     public string Name { get; }
-    internal static readonly Dictionary<string, List<Func<CEntityInstance, T?>>> Providers = new();
+    internal static readonly Dictionary<string, List<Func<CCSPlayerController, T?>>> Providers = new();
 
     public PlayerCapability(string name)
     {
         Name = name;
     }
 
-    public T? Get(CEntityInstance entity)
+    public T? Get(CCSPlayerController entity)
     {
         foreach (var provider in Providers[Name])
         {
