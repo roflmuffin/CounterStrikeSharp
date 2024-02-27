8a39762cf09a85bfa6edb04ff8f72ed81872e6ff
zonical <zonicalguy@gmail.com>
2024-02-27 20:50:00 +1300

    Adjust tabs

--- managed/CounterStrikeSharp.API/Utilities.cs
+++ managed/CounterStrikeSharp.API/Utilities.cs
@@ -28,216 +28,216 @@ using Microsoft.Extensions.Logging;
 
 namespace CounterStrikeSharp.API
 {
-    public static class Utilities
-    {
-        // https://github.com/dotabuff/manta/blob/master/entity.go#L186-L190
-        public const int MaxEdictBits = 15;
-        public const int MaxEdicts = 1 << MaxEdictBits;
-        public const int NumEHandleSerialNumberBits = 17;
-        public const uint InvalidEHandleIndex = 0xFFFFFFFF;
+	public static class Utilities
+	{
+		// https://github.com/dotabuff/manta/blob/master/entity.go#L186-L190
+		public const int MaxEdictBits = 15;
+		public const int MaxEdicts = 1 << MaxEdictBits;
+		public const int NumEHandleSerialNumberBits = 17;
+		public const uint InvalidEHandleIndex = 0xFFFFFFFF;
 
-        public static IEnumerable<T> FlagsToList<T>(this T flags) where T : Enum
-        {
-            return Enum.GetValues(typeof(T)).Cast<T>()
-                .Where(x => flags.HasFlag(x)).AsEnumerable();
-        }
+		public static IEnumerable<T> FlagsToList<T>(this T flags) where T : Enum
+		{
+			return Enum.GetValues(typeof(T)).Cast<T>()
+				.Where(x => flags.HasFlag(x)).AsEnumerable();
+		}
 
-        public static T GetEntityFromIndex<T>(int index) where T : CEntityInstance
-        {
-            return (T)Activator.CreateInstance(typeof(T), NativeAPI.GetEntityFromIndex(index))!;
-        }
+		public static T GetEntityFromIndex<T>(int index) where T : CEntityInstance
+		{
+			return (T)Activator.CreateInstance(typeof(T), NativeAPI.GetEntityFromIndex(index))!;
+		}
 
-        public static T? CreateEntityByName<T>(string name) where T : CBaseEntity
-        {
-            return (T?)Activator.CreateInstance(typeof(T), VirtualFunctions.UTIL_CreateEntityByName(name, -1));
-        }
+		public static T? CreateEntityByName<T>(string name) where T : CBaseEntity
+		{
+			return (T?)Activator.CreateInstance(typeof(T), VirtualFunctions.UTIL_CreateEntityByName(name, -1));
+		}
 
 		[Obsolete("Use Players.GetPlayerFromIndex instead")]
 		public static CCSPlayerController GetPlayerFromIndex(int index)
-        {
-            return Utilities.GetEntityFromIndex<CCSPlayerController>(index);
-        }
+		{
+			return Utilities.GetEntityFromIndex<CCSPlayerController>(index);
+		}
 
 		[Obsolete("Use Players.GetPlayerFromSlot instead")]
 		public static CCSPlayerController GetPlayerFromSlot(int slot)
-        {
-            return Utilities.GetEntityFromIndex<CCSPlayerController>(slot + 1);
-        }
+		{
+			return Utilities.GetEntityFromIndex<CCSPlayerController>(slot + 1);
+		}
 
 		[Obsolete("Use Players.GetPlayerFromUserid instead")]
 		public static CCSPlayerController GetPlayerFromUserid(int userid)
-        {
-            return Utilities.GetEntityFromIndex<CCSPlayerController>((userid & 0xFF) + 1);
-        }
+		{
+			return Utilities.GetEntityFromIndex<CCSPlayerController>((userid & 0xFF) + 1);
+		}
 
 		[Obsolete("Use Players.GetPlayerFromSteamId instead")]
 		public static CCSPlayerController? GetPlayerFromSteamId(ulong steamId)
-        {
-            return Utilities.GetPlayers().FirstOrDefault(player => player.AuthorizedSteamID == (SteamID)steamId);
-        }
+		{
+			return Utilities.GetPlayers().FirstOrDefault(player => player.AuthorizedSteamID == (SteamID)steamId);
+		}
 
-        public static TargetResult ProcessTargetString(string pattern, CCSPlayerController player)
-        {
-            return new Target(pattern).GetTarget(player);
-        }
+		public static TargetResult ProcessTargetString(string pattern, CCSPlayerController player)
+		{
+			return new Target(pattern).GetTarget(player);
+		}
 
-        public static bool RemoveItemByDesignerName(this CCSPlayerController player, string designerName)
-        {
-            return RemoveItemByDesignerName(player, designerName, false);
-        }
+		public static bool RemoveItemByDesignerName(this CCSPlayerController player, string designerName)
+		{
+			return RemoveItemByDesignerName(player, designerName, false);
+		}
 
-        public static bool RemoveItemByDesignerName(this CCSPlayerController player, string designerName, bool shouldRemoveEntity)
-        {
-            CHandle<CBasePlayerWeapon>? item = null;
-            if (player.PlayerPawn.Value == null || player.PlayerPawn.Value.WeaponServices == null) return false;
+		public static bool RemoveItemByDesignerName(this CCSPlayerController player, string designerName, bool shouldRemoveEntity)
+		{
+			CHandle<CBasePlayerWeapon>? item = null;
+			if (player.PlayerPawn.Value == null || player.PlayerPawn.Value.WeaponServices == null) return false;
 
-            foreach(var weapon in player.PlayerPawn.Value.WeaponServices.MyWeapons)
-            {
-                if (weapon is not { IsValid: true, Value.IsValid: true }) 
-                    continue;
-                if (weapon.Value.DesignerName != designerName) 
-                    continue;
+			foreach (var weapon in player.PlayerPawn.Value.WeaponServices.MyWeapons)
+			{
+				if (weapon is not { IsValid: true, Value.IsValid: true })
+					continue;
+				if (weapon.Value.DesignerName != designerName)
+					continue;
 
-                item = weapon;
-            }
-            
-            if (item != null && item.Value != null)
-            {
-                player.PlayerPawn.Value.RemovePlayerItem(item.Value);
+				item = weapon;
+			}
 
-                if (shouldRemoveEntity)
-                {
-                    item.Value.Remove();
-                }
+			if (item != null && item.Value != null)
+			{
+				player.PlayerPawn.Value.RemovePlayerItem(item.Value);
 
-                return true;
-            }
-            
-            return false;
-        }
+				if (shouldRemoveEntity)
+				{
+					item.Value.Remove();
+				}
 
-        public static IEnumerable<T> FindAllEntitiesByDesignerName<T>(string designerName) where T : CEntityInstance
-        {
-            var pEntity = new CEntityIdentity(EntitySystem.FirstActiveEntity);
-            for (; pEntity != null && pEntity.Handle != IntPtr.Zero; pEntity = pEntity.Next)
-            {
-                if (pEntity.DesignerName == null || !pEntity.DesignerName.Contains(designerName)) continue;
-                yield return new PointerTo<T>(pEntity.Handle).Value;
-            }
-        }
-        
-        public static IEnumerable<CEntityInstance> GetAllEntities()
-        {
-            var pEntity = new CEntityIdentity(EntitySystem.FirstActiveEntity);
-            for (; pEntity != null && pEntity.Handle != IntPtr.Zero; pEntity = pEntity.Next)
-            {
-                yield return new PointerTo<CEntityInstance>(pEntity.Handle).Value;
-            }
-        }
+				return true;
+			}
+
+			return false;
+		}
+
+		public static IEnumerable<T> FindAllEntitiesByDesignerName<T>(string designerName) where T : CEntityInstance
+		{
+			var pEntity = new CEntityIdentity(EntitySystem.FirstActiveEntity);
+			for (; pEntity != null && pEntity.Handle != IntPtr.Zero; pEntity = pEntity.Next)
+			{
+				if (pEntity.DesignerName == null || !pEntity.DesignerName.Contains(designerName)) continue;
+				yield return new PointerTo<T>(pEntity.Handle).Value;
+			}
+		}
+
+		public static IEnumerable<CEntityInstance> GetAllEntities()
+		{
+			var pEntity = new CEntityIdentity(EntitySystem.FirstActiveEntity);
+			for (; pEntity != null && pEntity.Handle != IntPtr.Zero; pEntity = pEntity.Next)
+			{
+				yield return new PointerTo<CEntityInstance>(pEntity.Handle).Value;
+			}
+		}
 
 		/// <summary>
 		/// Returns a list of <see cref="CCSPlayerController"/> that are valid and have a valid <see cref="CCSPlayerController.UserId"/> >= 0
 		/// </summary>
 		[Obsolete("Use Players.GetPlayers instead")]
 		public static List<CCSPlayerController> GetPlayers()
-        {
-            List<CCSPlayerController> players = new();
+		{
+			List<CCSPlayerController> players = new();
 
-            for (int i = 0; i < Server.MaxPlayers; i++)
-            {
-                var controller = GetPlayerFromSlot(i);
+			for (int i = 0; i < Server.MaxPlayers; i++)
+			{
+				var controller = GetPlayerFromSlot(i);
 
-                if (!controller.IsValid || controller.UserId == -1)
-                    continue;
+				if (!controller.IsValid || controller.UserId == -1)
+					continue;
 
-                players.Add(controller);
-            }
+				players.Add(controller);
+			}
 
-            return players;
-        }
+			return players;
+		}
 
-        [Obsolete]
-        public static void ReplyToCommand(CCSPlayerController? player, string msg, bool console = false)
-        {
-            if (player != null)
-            {
-                if (console) player.PrintToConsole(msg);
-                else player.PrintToChat(msg);
-            }
-            else
-            {
-                Server.PrintToConsole(msg);
-            }
-        }
+		[Obsolete]
+		public static void ReplyToCommand(CCSPlayerController? player, string msg, bool console = false)
+		{
+			if (player != null)
+			{
+				if (console) player.PrintToConsole(msg);
+				else player.PrintToChat(msg);
+			}
+			else
+			{
+				Server.PrintToConsole(msg);
+			}
+		}
 
-        public static unsafe string ReadStringUtf8(IntPtr ptr)
-        {
-            unsafe
-            {
-                var nativeUtf8 = Unsafe.Read<IntPtr>((void*)ptr);
+		public static unsafe string ReadStringUtf8(IntPtr ptr)
+		{
+			unsafe
+			{
+				var nativeUtf8 = Unsafe.Read<IntPtr>((void*)ptr);
 
-                if (nativeUtf8 == IntPtr.Zero)
-                {
-                    return null;
-                }
+				if (nativeUtf8 == IntPtr.Zero)
+				{
+					return null;
+				}
 
-                var len = 0;
-                while (Marshal.ReadByte(nativeUtf8, len) != 0)
-                {
-                    ++len;
-                }
+				var len = 0;
+				while (Marshal.ReadByte(nativeUtf8, len) != 0)
+				{
+					++len;
+				}
 
-                var buffer = new byte[len];
-                Marshal.Copy(nativeUtf8, buffer, 0, buffer.Length);
-                return Encoding.UTF8.GetString(buffer);
-            }
-        }
+				var buffer = new byte[len];
+				Marshal.Copy(nativeUtf8, buffer, 0, buffer.Length);
+				return Encoding.UTF8.GetString(buffer);
+			}
+		}
 
-        public static T? GetPointer<T>(IntPtr pointer) where T : NativeObject
-        {
-            var pointerTo = Marshal.ReadIntPtr(pointer);
-            if (pointerTo == IntPtr.Zero)
-            {
-                return null;
-            }
+		public static T? GetPointer<T>(IntPtr pointer) where T : NativeObject
+		{
+			var pointerTo = Marshal.ReadIntPtr(pointer);
+			if (pointerTo == IntPtr.Zero)
+			{
+				return null;
+			}
 
-            return (T)Activator.CreateInstance(typeof(T), pointerTo)!;
-        }
-        
-        private static int FindSchemaChain(string className) => Schema.GetSchemaOffset(className, "__m_pChainEntity");
+			return (T)Activator.CreateInstance(typeof(T), pointerTo)!;
+		}
 
-        /// <summary>
-        /// Marks a field as changed for network transmission.
-        /// Not all schema fields are network enabled, so please check the schema before using this.
-        /// </summary>
-        /// <param name="entity">Entity to update</param>
-        /// <param name="className" example="CBaseEntity">Schema field class name</param>
-        /// <param name="fieldName" example="m_iHealth">Schema field name</param>
-        /// <param name="extraOffset">Any additional offset to the schema field</param>
-        /// <exception cref="InvalidOperationException">Entity is not valid</exception>
-        public static void SetStateChanged(CBaseEntity entity, string className, string fieldName, int extraOffset = 0)
-        {
-            Guard.IsValidEntity(entity);
+		private static int FindSchemaChain(string className) => Schema.GetSchemaOffset(className, "__m_pChainEntity");
 
-            if (!Schema.IsSchemaFieldNetworked(className, fieldName))
-            {
-                Application.Instance.Logger.LogWarning("Field {ClassName}:{FieldName} is not networked, but SetStateChanged was called on it.", className, fieldName);
-                return;
-            }
-            
-            int offset = Schema.GetSchemaOffset(className, fieldName);
-            int chainOffset = FindSchemaChain(className);
+		/// <summary>
+		/// Marks a field as changed for network transmission.
+		/// Not all schema fields are network enabled, so please check the schema before using this.
+		/// </summary>
+		/// <param name="entity">Entity to update</param>
+		/// <param name="className" example="CBaseEntity">Schema field class name</param>
+		/// <param name="fieldName" example="m_iHealth">Schema field name</param>
+		/// <param name="extraOffset">Any additional offset to the schema field</param>
+		/// <exception cref="InvalidOperationException">Entity is not valid</exception>
+		public static void SetStateChanged(CBaseEntity entity, string className, string fieldName, int extraOffset = 0)
+		{
+			Guard.IsValidEntity(entity);
 
-            if (chainOffset != 0)
-            {
-                VirtualFunctions.NetworkStateChanged(entity.Handle + chainOffset, offset + extraOffset, 0xFFFFFFFF);
-                return;
-            }
+			if (!Schema.IsSchemaFieldNetworked(className, fieldName))
+			{
+				Application.Instance.Logger.LogWarning("Field {ClassName}:{FieldName} is not networked, but SetStateChanged was called on it.", className, fieldName);
+				return;
+			}
 
-            VirtualFunctions.StateChanged(entity.NetworkTransmitComponent.Handle, entity.Handle, offset + extraOffset, -1, -1);
+			int offset = Schema.GetSchemaOffset(className, fieldName);
+			int chainOffset = FindSchemaChain(className);
 
-            entity.LastNetworkChange = Server.CurrentTime;
-            entity.IsSteadyState.Clear();
-        }
-    }
+			if (chainOffset != 0)
+			{
+				VirtualFunctions.NetworkStateChanged(entity.Handle + chainOffset, offset + extraOffset, 0xFFFFFFFF);
+				return;
+			}
+
+			VirtualFunctions.StateChanged(entity.NetworkTransmitComponent.Handle, entity.Handle, offset + extraOffset, -1, -1);
+
+			entity.LastNetworkChange = Server.CurrentTime;
+			entity.IsSteadyState.Clear();
+		}
+	}
 }
