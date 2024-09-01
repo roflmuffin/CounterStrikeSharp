d8b10b9c804122968db86c7d890f6c0b1e09f82e
zonical <zonicalguy@gmail.com>
2024-09-02 09:01:55 +1200

    Add CUtlSymbolLarge

---
+++ managed/CounterStrikeSharp.API/Core/Model/CUtlSymbolLarge.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection.Metadata;
+
+namespace CounterStrikeSharp.API.Core;
+
+public class CUtlSymbolLarge : NativeObject
+{
+    public CUtlSymbolLarge(IntPtr pointer) : base(pointer)
+    {
+    }
+
+    public bool IsValid => Handle != IntPtr.Zero;
+
+    public string String
+    {
+        get
+        {
+            return NativeAPI.GetStringFromSymbolLarge(Handle);
+        }
+    }
+}
