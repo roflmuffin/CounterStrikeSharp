ce27ee7610c9d6d60ae0de10319081aa5237e208
KillStr3aK <KillStr3aK@users.noreply.github.com>
2024-10-08 13:38:13 +0200

    feat: these should be `DisposableMemory`

--- managed/CounterStrikeSharp.API/Modules/Utils/Quaternion.cs
+++ managed/CounterStrikeSharp.API/Modules/Utils/Quaternion.cs
@@ -14,13 +14,12 @@
  *  along with CounterStrikeSharp.  If not, see <https://www.gnu.org/licenses/>. *
  */
 
-using System;
 using System.Runtime.CompilerServices;
-using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Memory;
 
 namespace CounterStrikeSharp.API.Modules.Utils
 {
-    public class Quaternion : NativeObject
+    public class Quaternion : DisposableMemory
     {
         public Quaternion(IntPtr pointer) : base(pointer)
         {
