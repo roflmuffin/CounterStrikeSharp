d02384d7913e408b3a03dbb70fb89293046e4198
schwarper <75811921+schwarper@users.noreply.github.com>
2024-11-10 12:14:49 +0300

    Created a new partial class for CTakeDamageInfo

---
+++ managed/CounterStrikeSharp.API/Core/Model/CTakeDamageInfo.cs
@@ -0,0 +1,30 @@
+﻿namespace CounterStrikeSharp.API.Core;
+
+public partial class CTakeDamageInfo
+{
+    /// <summary>
+    /// Retrieves the hitgroup
+    /// </summary>
+    /// <returns>
+    /// Returns a <see cref="HitGroup_t"/> enumeration representing the player's current hit group,
+    /// or <see cref="HitGroup_t.HITGROUP_INVALID"/> if the hit group cannot be determined.
+    /// </returns>
+    public unsafe HitGroup_t HitGroup()
+    {
+        nint v4 = *(nint*)(this.Handle + 0x78);
+
+        if (v4 == nint.Zero)
+        {
+            return HitGroup_t.HITGROUP_INVALID;
+        }
+
+        nint v1 = *(nint*)(v4 + 16);
+
+        if (v1 == nint.Zero)
+        {
+            return HitGroup_t.HITGROUP_GENERIC;
+        }
+
+        return (HitGroup_t)(*(uint*)(v1 + 56));
+    }
+}
--- managed/CounterStrikeSharp.API/Core/Schema/Classes/CTakeDamageInfo.g.cs
+++ managed/CounterStrikeSharp.API/Core/Schema/Classes/CTakeDamageInfo.g.cs
@@ -102,30 +102,4 @@ public partial class CTakeDamageInfo : NativeObject
 	// m_bInTakeDamageFlow
 	[SchemaMember("CTakeDamageInfo", "m_bInTakeDamageFlow")]
 	public ref bool InTakeDamageFlow => ref Schema.GetRef<bool>(this.Handle, "CTakeDamageInfo", "m_bInTakeDamageFlow");
-
-    /// <summary>
-    /// Retrieves the hitgroup
-    /// </summary>
-    /// <returns>
-    /// Returns a <see cref="HitGroup_t"/> enumeration representing the player's current hit group,
-    /// or <see cref="HitGroup_t.HITGROUP_INVALID"/> if the hit group cannot be determined.
-    /// </returns>
-    public unsafe HitGroup_t HitGroup()
-    {
-        nint v4 = *(nint*)(this.Handle + 0x78);
-
-        if (v4 == nint.Zero)
-        {
-            return HitGroup_t.HITGROUP_INVALID;
-        }
-
-        nint v1 = *(nint*)(v4 + 16);
-
-        if (v1 == nint.Zero)
-        {
-            return HitGroup_t.HITGROUP_GENERIC;
-        }
-
-        return (HitGroup_t)(*(uint*)(v1 + 56));
-    }
 }
