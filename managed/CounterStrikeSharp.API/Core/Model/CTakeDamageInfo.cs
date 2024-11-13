78be4e58e1dd829217e5aea3de8c4c1b610f801e
schwarper <75811921+schwarper@users.noreply.github.com>
2024-11-13 22:20:45 +0300

    Converted nint to safe codes

--- managed/CounterStrikeSharp.API/Core/Model/CTakeDamageInfo.cs
+++ managed/CounterStrikeSharp.API/Core/Model/CTakeDamageInfo.cs
@@ -1,4 +1,6 @@
-﻿namespace CounterStrikeSharp.API.Core;
+﻿using System.Runtime.InteropServices;
+
+namespace CounterStrikeSharp.API.Core;
 
 public partial class CTakeDamageInfo
 {
@@ -9,22 +11,22 @@ public partial class CTakeDamageInfo
     /// Returns a <see cref="HitGroup_t"/> enumeration representing the player's current hit group,
     /// or <see cref="HitGroup_t.HITGROUP_INVALID"/> if the hit group cannot be determined.
     /// </returns>
-    public unsafe HitGroup_t HitGroup()
+    public HitGroup_t HitGroup()
     {
-        nint v4 = *(nint*)(this.Handle + 0x78);
+        IntPtr v4 = Marshal.ReadIntPtr(Handle, 0x78);
 
         if (v4 == nint.Zero)
         {
             return HitGroup_t.HITGROUP_INVALID;
         }
 
-        nint v1 = *(nint*)(v4 + 16);
+        IntPtr v1 = Marshal.ReadIntPtr(v4, 16);
 
         if (v1 == nint.Zero)
         {
             return HitGroup_t.HITGROUP_GENERIC;
         }
 
-        return (HitGroup_t)(*(uint*)(v1 + 56));
+        return (HitGroup_t)Marshal.ReadInt32(v1, 56);
     }
 }
