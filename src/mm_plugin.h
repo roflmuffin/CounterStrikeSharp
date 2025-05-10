462ca52229d859b593f02e3001d2c819877352af
Michael Wilson <roflmuffin@users.noreply.github.com>
2025-05-10 12:17:45 +1000

    chore(deps): upgrade metamod & hl2sdk (#856)

--- src/mm_plugin.h
+++ src/mm_plugin.h
@@ -74,7 +74,9 @@ static ScriptCallback* on_activate_callback;
 static ScriptCallback* on_metamod_all_plugins_loaded_callback;
 extern CounterStrikeSharpMMPlugin gPlugin;
 
-PLUGIN_GLOBALVARS();
 
 #endif //_INCLUDE_METAMOD_SOURCE_STUB_PLUGIN_H_
 }
+
+PLUGIN_GLOBALVARS();
+
