9d6e57c33c318c32daab5931f072e3db32354ee8
Michael Wilson <roflmuffin@users.noreply.github.com>
2025-07-07 08:05:50 +0000

    feat: wip add xunit test plugin

---
+++ managed/CounterStrikeSharp.Tests.Native/CommandTests.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Threading.Tasks;
+using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using Moq;
+using Xunit;
+
+namespace NativeTestsPlugin;
+
+public class CommandTests
+{
+    [Fact]
+    public async Task AddCommandHandler()
+    {
+        var mock = new Mock<Action>();
+        var methodCallback = FunctionReference.Create(() =>
+        {
+            mock.Object.Invoke();
+        });
+
+        NativeAPI.AddCommand("test_native", "description", true, (int)ConCommandFlags.FCVAR_LINKED_CONCOMMAND, methodCallback);
+        NativeAPI.IssueServerCommand("test_native");
+        await WaitOneFrame();
+        mock.Verify(s => s(), Times.Once);
+
+        NativeAPI.RemoveCommand("test_native", methodCallback);
+        NativeAPI.IssueServerCommand("test_native");
+        await WaitOneFrame();
+        mock.Verify(s => s(), Times.Once);
+    }
+
+    [Fact(Skip = "This test is not suitable for automated testing.")]
+    public async Task IssueServerCommand()
+    {
+        bool called = false;
+        NativeAPI.AddCommandListener("say", FunctionReference.Create(() =>
+        {
+            called = true;
+        }), true);
+
+        NativeAPI.IssueServerCommand("say Hello, world!");
+        await WaitOneFrame();
+
+        Assert.True(called, "The 'say' command was issued successfully.");
+    }
+
+    private async Task WaitOneFrame()
+    {
+        await Server.RunOnTickAsync(Server.TickCount + 2, () => { }).ConfigureAwait(false);
+    }
+}
