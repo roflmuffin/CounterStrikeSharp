22809e3ec6aebacfa707f79ac39a2d740f92d23b
Michael Wilson <roflmuffin@users.noreply.github.com>
2025-07-07 14:14:28 +1200

    feat: update schema for game update 1.40.8.5 (03.07.2025) (#921)

--- managed/CounterStrikeSharp.API/Core/Schema/Enums/CSPlayerBlockingUseAction_t.g.cs
+++ managed/CounterStrikeSharp.API/Core/Schema/Enums/CSPlayerBlockingUseAction_t.g.cs
@@ -13,9 +13,7 @@ public enum CSPlayerBlockingUseAction_t : uint
 	k_CSPlayerBlockingUseAction_DefusingWithKit = 0x2,
 	k_CSPlayerBlockingUseAction_HostageGrabbing = 0x3,
 	k_CSPlayerBlockingUseAction_HostageDropping = 0x4,
-	k_CSPlayerBlockingUseAction_EquippingHeavyArmor = 0x5,
-	k_CSPlayerBlockingUseAction_TakingOffHeavyArmor = 0x6,
-	k_CSPlayerBlockingUseAction_MapLongUseEntity_Pickup = 0x7,
-	k_CSPlayerBlockingUseAction_MapLongUseEntity_Place = 0x8,
-	k_CSPlayerBlockingUseAction_MaxCount = 0x9,
+	k_CSPlayerBlockingUseAction_MapLongUseEntity_Pickup = 0x5,
+	k_CSPlayerBlockingUseAction_MapLongUseEntity_Place = 0x6,
+	k_CSPlayerBlockingUseAction_MaxCount = 0x7,
 }
