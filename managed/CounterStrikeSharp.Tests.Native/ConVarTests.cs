f7cd192645066c31662884779ff23bcd5362af5e
Michael Wilson <roflmuffin@users.noreply.github.com>
2025-07-14 09:01:01 +0000

    fix: tests

--- managed/CounterStrikeSharp.Tests.Native/ConVarTests.cs
+++ managed/CounterStrikeSharp.Tests.Native/ConVarTests.cs
@@ -193,7 +193,7 @@ public class ConVarTests
         // Test min/max constraints
         conVar.Value = 50.0f;
         Assert.Equal(25.0f, conVar.Value);
-        Assert.Equal("25.0", conVar.ValueAsString);
+        Assert.Equal("25.000000", conVar.ValueAsString);
 
         conVar.ValueAsString = "10.5";
         Assert.Equal(10.5f, conVar.Value);
