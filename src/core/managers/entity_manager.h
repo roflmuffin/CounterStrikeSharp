07943ab5c2c9e7bf9d894181ae08d21330683138
samyyc <3356207189@qq.com>
2025-08-04 22:58:25 +0800

    fix: test

--- src/core/managers/entity_manager.h
+++ src/core/managers/entity_manager.h
@@ -231,6 +231,7 @@ struct SndOpEventGuid_t
 {
     SoundEventGuid_t m_nGuid;
     uint64 m_hStackHash;
+    uint64 pad;
 };
 
 class CBitRecipientFilter
@@ -251,7 +252,10 @@ class CBitRecipientFilter
     NetChannelBufType_t m_nBufType;
 };
 
-inline SndOpEventGuid_t(FASTCALL* CBaseEntity_EmitSoundFilter)(CBitRecipientFilter& filter, CEntityIndex ent, const EmitSound_t& params);
+inline SndOpEventGuid_t*(FASTCALL* CBaseEntity_EmitSoundFilter)(SndOpEventGuid_t& ret,
+                                                                CBitRecipientFilter& filter,
+                                                                CEntityIndex ent,
+                                                                const EmitSound_t& params);
 
 SndOpEventGuid_t
 EntityEmitSoundFilter(CBitRecipientFilter& filter, uint32 ent, const char* pszSound, float flVolume = 1.0f, float flPitch = 1.0f);
