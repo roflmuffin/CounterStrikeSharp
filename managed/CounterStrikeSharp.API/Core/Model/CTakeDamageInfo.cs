51b6b45390f9562a3f0d8bdc34af474b28a0e8b1
Michael Wilson <roflmuffin@users.noreply.github.com>
2025-08-16 00:35:58 +0000

    fix: obsolete `GetHitGroup()` and point to `HitGroupId` schema property

--- managed/CounterStrikeSharp.API/Core/Model/CTakeDamageInfo.cs
+++ managed/CounterStrikeSharp.API/Core/Model/CTakeDamageInfo.cs
@@ -11,22 +11,9 @@ public partial class CTakeDamageInfo
     /// Returns a <see cref="HitGroup_t"/> enumeration representing the player's current hit group,
     /// or <see cref="HitGroup_t.HITGROUP_INVALID"/> if the hit group cannot be determined.
     /// </returns>
+    [Obsolete("Use HitGroupId instead.")]
     public HitGroup_t GetHitGroup()
     {
-        IntPtr v4 = Marshal.ReadIntPtr(Handle, 0x70);
-
-        if (v4 == nint.Zero)
-        {
-            return HitGroup_t.HITGROUP_INVALID;
-        }
-
-        IntPtr v1 = Marshal.ReadIntPtr(v4, 16);
-
-        if (v1 == nint.Zero)
-        {
-            return HitGroup_t.HITGROUP_GENERIC;
-        }
-
-        return (HitGroup_t)Marshal.ReadInt32(v1, 56);
+        return HitGroupId;
     }
 }
