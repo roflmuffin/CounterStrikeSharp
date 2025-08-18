624ca0f0de35da5fd7f09a7498860a2f697ff59c
Michael Wilson <roflmuffin@users.noreply.github.com>
2025-08-18 01:50:39 +0000

    fix: re-add GetHitGroup with GameData backed offset

--- managed/CounterStrikeSharp.API/Core/Model/CTakeDamageInfo.cs
+++ managed/CounterStrikeSharp.API/Core/Model/CTakeDamageInfo.cs
@@ -11,9 +11,22 @@ public partial class CTakeDamageInfo
     /// Returns a <see cref="HitGroup_t"/> enumeration representing the player's current hit group,
     /// or <see cref="HitGroup_t.HITGROUP_INVALID"/> if the hit group cannot be determined.
     /// </returns>
-    [Obsolete("Use HitGroupId instead.")]
     public HitGroup_t GetHitGroup()
     {
-        return HitGroupId;
+        IntPtr v4 = Marshal.ReadIntPtr(Handle, GameData.GetOffset("CTakeDamageInfo_HitGroup"));
+
+        if (v4 == nint.Zero)
+        {
+            return HitGroup_t.HITGROUP_INVALID;
+        }
+
+        IntPtr v1 = Marshal.ReadIntPtr(v4, 16);
+
+        if (v1 == nint.Zero)
+        {
+            return HitGroup_t.HITGROUP_GENERIC;
+        }
+
+        return (HitGroup_t)Marshal.ReadInt32(v1, 56);
     }
 }
