67a26483540fee72f8c2750b38472d14ee9f31f9
Ambr0se <eldment@qq.com>
2025-12-27 11:53:56 +0800

    fix: Ensure compatibility with CS2Fixes

--- src/core/managers/entity_manager.cpp
+++ src/core/managers/entity_manager.cpp
@@ -15,7 +15,7 @@
  */
 
 #include "core/managers/entity_manager.h"
-#include "core/detours.h"
+// #include "core/detours.h"
 #include "core/function.h"
 #include "core/gameconfig.h"
 #include "core/globals.h"
@@ -129,7 +129,7 @@ void EntityManager::OnAllInitialized()
         new ValveFunction((void*)CBaseEntity_TakeDamageOld, CALL_CONV,
                           std::vector<DataType_t>{ DATA_TYPE_POINTER, DATA_TYPE_POINTER, DATA_TYPE_POINTER }, DATA_TYPE_LONG_LONG);
 
-    Func_OnTakeDamage->AddHook(&OnTakeDamageProxy);
+    // Func_OnTakeDamage->AddHook(&OnTakeDamageProxy);
 
     auto m_hook = funchook_create();
     funchook_prepare(m_hook, (void**)&m_pFireOutputInternal, (void*)&DetourFireOutputInternal);
