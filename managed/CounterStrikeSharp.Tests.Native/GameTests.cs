2c74f901836c78f4d9cfc7476decc735823e9fe3
roflmuffin <shortguy014@gmail.com>
2026-01-23 09:29:32 +0000

    fix: update gamedata offsets, add more tests

--- managed/CounterStrikeSharp.Tests.Native/GameTests.cs
+++ managed/CounterStrikeSharp.Tests.Native/GameTests.cs
@@ -3,6 +3,7 @@ using System.Numerics;
 using System.Threading.Tasks;
 using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Entities.Constants;
 using CounterStrikeSharp.API.Modules.Utils;
 using Xunit;
 
@@ -11,17 +12,25 @@ namespace NativeTestsPlugin;
 public class GameTests
 {
     private CCSPlayerController player;
-    private readonly CCSPlayerPawn? pawn;
+    private CCSPlayerPawn? pawn;
 
-    public GameTests()
+    public async Task InitializeAsync()
     {
-        this.player = Utilities.GetPlayers().First(p => p.LifeState == (byte)LifeState_t.LIFE_ALIVE);
+        Server.ExecuteCommand("bot_kick; bot_quota 5; bot_quota_mode normal");
+        await WaitOneFrame();
+        this.player = Utilities.GetPlayers().Last(p => p.LifeState == (byte)LifeState_t.LIFE_ALIVE);
+        if (player.PlayerPawn.Value == null)
+        {
+            throw new Exception("No valid player pawn found for test player.");
+        }
+
         this.pawn = player.PlayerPawn.Value;
     }
 
     [Fact]
     public async Task Offset_CBasePlayerPawn_CommitSuicide()
     {
+        await InitializeAsync();
         Assert.Equal((byte)LifeState_t.LIFE_ALIVE, pawn.LifeState);
 
         pawn.CommitSuicide(true, true);
@@ -32,6 +41,7 @@ public class GameTests
     [Fact]
     public async Task Offset_CCSPlayer_ItemServices_RemoveWeapons()
     {
+        await InitializeAsync();
         Assert.True(pawn.WeaponServices.MyWeapons.Any());
 
         player.RemoveWeapons();
@@ -43,18 +53,22 @@ public class GameTests
     [Fact]
     public async Task Offset_CBaseEntity_Teleport()
     {
+        await InitializeAsync();
         var originalPosition = pawn.AbsOrigin;
         var newPosition = (Vector3)originalPosition + new Vector3(0, 0, 100);
 
         pawn.Teleport(newPosition, null, null);
         await WaitOneFrame();
 
-        Assert.Equal(newPosition, (Vector3)pawn.AbsOrigin);
+        Assert.Equal(newPosition.X, pawn.AbsOrigin.X, 1f);
+        Assert.Equal(newPosition.Y, pawn.AbsOrigin.Y, 1f);
+        Assert.Equal(newPosition.Z, pawn.AbsOrigin.Z, 1f);
     }
 
     [Fact]
     public async Task Offset_CCSPlayerController_ChangeTeam()
     {
+        await InitializeAsync();
         var originalTeam = player.Team;
         var newTeam = originalTeam == CsTeam.Terrorist ? CsTeam.CounterTerrorist : CsTeam.Terrorist;
 
@@ -62,5 +76,23 @@ public class GameTests
         await WaitOneFrame();
 
         Assert.Equal(newTeam, player.Team);
+    }
+
+    [Fact]
+    public async Task Offset_CCSPlayer_ItemServices_GiveNamedItem()
+    {
+        await InitializeAsync();
+
+        player.RemoveWeapons();
+        await WaitOneFrame();
+
+        Assert.False(pawn.WeaponServices.MyWeapons.Any());
+
+        var weapon = player.GiveNamedItem<CMolotovGrenade>("weapon_ak47");
+        await WaitOneFrame();
+
+        Assert.NotNull(weapon);
+        Assert.Equal("weapon_ak47", weapon.DesignerName);
+        Assert.Single(pawn.WeaponServices.MyWeapons);
     }
 }
